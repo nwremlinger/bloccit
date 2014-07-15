da58b9ab4970469c5dd8b01b330cfa10e969571d
Nick Remlinger <nwremlinger@gmail.com>
2014-07-15 02:46:41 +0000

    Associated & nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,16 @@ require 'faker'
 end
 users = User.all
 
+ # Create Topics
+ 15.times do
+   Topic.create(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
+
+
 # Note: by calling `User.new` instead of `create`,
 # we create an instance of User which isn't immediately saved to the database.
 
@@ -25,6 +35,7 @@ users = User.all
 50.times do
   Post.create(
     user:   users.sample,
+     topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -40,10 +51,35 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes(
-  email: 'nwremlinger@gmail.com',
-  password: '123456',
-)
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save
+
 
 
 
